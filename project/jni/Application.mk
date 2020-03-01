9b407ae4704f496226f1deeea24ee5673c14fd5d
amimo <61569276@qq.com>
2020-03-01 14:55:00 +0800

    build arm64-v8a by default

--- project/jni/Application.mk
+++ project/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL := c++_static
 APP_CPPFLAGS += -fvisibility=hidden
 APP_PLATFORM := android-19
-#APP_ABI := armeabi-v7a arm64-v8a
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi-v7a arm64-v8a
+#APP_ABI := armeabi-v7a
